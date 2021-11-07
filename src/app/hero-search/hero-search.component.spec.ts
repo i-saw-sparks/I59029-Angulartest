b55a5142af2dc672a8454bdd692101b3b87cd010
Andres Molina <andresmolinaceti@gmail.com>
2021-11-06 20:51:54 -0600

    exmaple completed

---
+++ src/app/hero-search/hero-search.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeroSearchComponentComponent } from './hero-search-component.component';
+
+describe('HeroSearchComponentComponent', () => {
+  let component: HeroSearchComponentComponent;
+  let fixture: ComponentFixture<HeroSearchComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HeroSearchComponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeroSearchComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
